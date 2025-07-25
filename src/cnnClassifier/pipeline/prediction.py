b41f213fab3deac3a3cdbd25c888a85cec77b448
srinivasaguru1 <srinivasaguru301@gmail.com>
2025-07-26 00:10:06 +0530

    update code

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tensorflow.keras.models import load_model
+#from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.preprocessing import image
 import os
 
@@ -13,8 +14,7 @@ class PredictionPipeline:
     
     def predict(self):
         # load model
-        model = load_model(os.path.join("model", "model.h5"))
-
+        model = load_model(os.path.join("model", "model.h5"),compile=False)
         imagename = self.filename
         test_image = image.load_img(imagename, target_size = (224,224))
         test_image = image.img_to_array(test_image)
